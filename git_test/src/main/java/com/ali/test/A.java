407d3515f9086073bf81bbda5e5151e6418f928f
Mr.he <2601473413@qq.com>
2019-12-23 20:30:36 +0800

    修改

--- git_test/src/main/java/com/ali/test/A.java
+++ git_test/src/main/java/com/ali/test/A.java
@@ -4,5 +4,6 @@ public class A {
     public static void main(String[] args) {
         System.out.println("12131313");
         System.out.println("1211131313s");
+        System.out.println("12121131313");
     }
 }
